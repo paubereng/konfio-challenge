bfde267335a45122b8ce50e87aebf9073cb0d4f6
paubereng <pauberengueras@gmail.com>
2019-06-23 18:03:20 +0200

    Called module hot.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <div><h1>konfio-App</h1></div>;
+const App = () => <div><h1>konfio-App reload</h1></div>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if(module.hot){
+  module.hot.accept();
+}
