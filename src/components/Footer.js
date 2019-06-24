fece224ced459c837309ae8ae25d116137e9b1f7
paubereng <pauberengueras@gmail.com>
2019-06-24 22:06:20 +0200

    Created footer component.

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="container has-text-centered">
+      <div>2019 <i className="fa fa-copyright"></i> Pau Berengueras. Made in Barcelona</div>
+    </div>
+  </footer>
+);
+
+export default Footer;
