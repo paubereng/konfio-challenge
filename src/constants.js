508a6672023f8708b01f90f014447dc9449cdabb
paubereng <pauberengueras@gmail.com>
2019-06-26 00:21:03 +0200

    Added url api, api key, base currency, initial currency and colors.

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,35 @@
-const CURRENCIES = [
-  'MEX', 'AUD', 'CHF', 'USD',
+export const BASE_URL = 'http://data.fixer.io/api/';
+
+export const API_KEY = '5b61b73b560cc3b4357f4e999cf160c5';
+
+export const BASE_CURRENCY = 'MXN';
+
+export const CURRENCIES = [
+  'USD', 'EUR', 'GBP',
 ];
 
-export default CURRENCIES;
+export const INITIAL_CURRENCIES_SELECT = [
+  {
+    label: 'USD', value: 'United States Dollar',
+  },
+  {
+    label: 'EUR', value: 'Euro',
+  },
+  {
+    label: 'GBP', value: 'British Pound Sterling',
+  },
+];
+
+export const COLORS = [
+  '#1abc9c',
+  '#f1c40f',
+  '#f39c12',
+  '#2ecc71',
+  '#27ae60',
+  '#e67e22',
+  '#d35400',
+  '#3498db',
+  '#e74c3c',
+  '#9b59b6',
+  '#95a5a6',
+];
