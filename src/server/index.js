43796f90391af5db71353fb8a543a8d291d7402b
paubereng <pauberengueras@gmail.com>
2019-06-27 03:32:40 +0200

    Solved problem finding dist folder.

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 const routes = require('./routes');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname, '../../dist/')));
 
 app.use(routes);
 app.listen(port);
